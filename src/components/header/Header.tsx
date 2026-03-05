c1ae6efaf9af6d3baa077e4bfc913648ac1d0791
Divansir-Junior <jscrobut989@gmail.com>
2026-03-05 00:04:23 -0300

    Header created

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div className="flex items-center justify-center bg-white p-4 fixed top-0 w-full h-16">
+      <h1 className="font-['Minecraft'] text-5xl">HypixelStats</h1>
+    </div>
+  );
+}
