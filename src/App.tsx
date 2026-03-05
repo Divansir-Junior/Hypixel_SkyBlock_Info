1147555727a2d308821abb4def7288e65e43d8e2
Divansir-Junior <jscrobut989@gmail.com>
2026-03-05 01:06:39 -0300

    feat: add SearchButton component and install lucide-react

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import Header from "./components/header/Header";
 import Container from "./components/Container/Container";
 import SearchBar from "./components/SearchBar/SearchBar";
+import SearchButton from "./components/SearchButton/SearchButton";
 export default function App() {
   return (
     <div>
       <Header />
       <Container>
         <SearchBar></SearchBar>
+        <SearchButton></SearchButton>
       </Container>
     </div>
   );
---
+++ src/components/SearchButton/SearchButton.tsx
@@ -0,0 +1,25 @@
+import { Search } from "lucide-react";
+
+export default function SearchButton() {
+  return (
+    <button
+      className="
+        border-2
+        ml-4
+        p-0.5
+        font-['Minecraft']
+        flex items-center justify-center
+        gap-2
+        text-black
+        cursor-pointer
+        bg-amber-50
+        transition-all duration-300 ease-in-out
+        hover:text-green-500
+        hover:bg-white
+      "
+    >
+      <Search size={20} />
+      <span>Search</span>
+    </button>
+  );
+}
