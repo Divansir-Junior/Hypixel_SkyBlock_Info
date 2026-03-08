afe2ba013b2f6e086ac409d51a167d55517a3b4d
Divansir-Junior <jscrobut989@gmail.com>
2026-03-08 02:28:54 -0300

    chore : removed carousel import from main tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 // App.tsx
 import Header from "./components/Header/Header";
 import HeroSection from "./components/HeroSection/HeroSection";
-import GameCarousel from "./components/Carousel/Carousel";
+
 import InfoSection from "./components/InfoSection/InfoSection";
 export default function App() {
   return (
