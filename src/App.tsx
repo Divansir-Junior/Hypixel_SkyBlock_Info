f00d1105265c20ca15d8997785ac0f4d84682a8d
Divansir-Junior <jscrobut989@gmail.com>
2026-03-09 16:50:15 -0300

    feat : Footer created

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import About from "./pages/About/About";
 import Header from "./components/Header/Header";
 import HeroSection from "./components/HeroSection/HeroSection";
 import InfoSection from "./components/InfoSection/InfoSection";
-
+import Footer from "./components/Footer/Footer";
 export default function App() {
   return (
     <div>
@@ -20,6 +20,7 @@ export default function App() {
         />
         <Route path="/about" element={<About />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="w-full h-5 bg-amber-950 text-center relative bottom-0">
+      <h1 className="font-['Minecraft'] text-white">Hypixel Stats 2026 </h1>
+    </footer>
+  );
+}
