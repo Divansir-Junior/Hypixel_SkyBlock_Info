a08d5d25b4a0dbd2d9a1f874ec12b748ec72e76d
Divansir-Junior <jscrobut989@gmail.com>
2026-03-09 02:49:04 -0300

    style : bg changed to  dark oak

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -6,7 +6,16 @@ interface Props {
 
 export default function Container({ children }: Props) {
   return (
-    <div className="w-full h-[500px] bg-amber-900 flex items-center justify-center">
+    <div
+      className="w-full h-[500px] flex items-center justify-center"
+      style={{
+        backgroundImage:
+          "url('https://ccvaults.com/textures/dark_oak_planks.png')",
+        backgroundRepeat: "repeat",
+        backgroundSize: "64px 64px",
+        imageRendering: "pixelated",
+      }}
+    >
       {children}
     </div>
   );
