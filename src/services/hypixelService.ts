5fa806b76d0bab10efd1ac555c227f7c72962419
Divansir-Junior <jscrobut989@gmail.com>
2026-03-10 00:02:37 -0300

    testing key bug ver 1.0

--- src/services/hypixelService.ts
+++ src/services/hypixelService.ts
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 export async function searchHypixelPlayer(uuid: string) {
-  const { data } = await axios.get(`/api/hypixel?uuid=${uuid}`);
+  const { data } = await axios.get(
+    `https://api.hypixel.net/v2/skyblock/profiles?uuid=${uuid}`,
+    { headers: { "API-Key": import.meta.env.VITE_HYPIXEL_API_KEY } },
+  );
   return data;
 }
