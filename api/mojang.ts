98730d440b60d233854521b746a9a98e99e1a9dd
Divansir-Junior <jscrobut989@gmail.com>
2026-03-10 06:03:47 -0300

    fix : import to the vercel

--- api/mojang.ts
+++ api/mojang.ts
@@ -3,7 +3,6 @@ import type { VercelRequest, VercelResponse } from "@vercel/node";
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   const { username } = req.query;
   if (!username) return res.status(400).json({ error: "Username is required" });
-
   const response = await fetch(
     `https://api.mojang.com/users/profiles/minecraft/${username}`,
   );
