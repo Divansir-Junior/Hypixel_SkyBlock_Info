fdbfe3e6d7cd55352d1d10a68968fabe43efd159
Divansir-Junior <jscrobut989@gmail.com>
2026-03-10 06:05:20 -0300

    fix : vercel imports

--- api/hypixel.ts
+++ api/hypixel.ts
@@ -1,6 +1,7 @@
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   const { uuid } = req.query;
-  console.log("API KEY:", process.env.HYPIXEL_API_KEY); // log temporário
   if (!uuid) return res.status(400).json({ error: "UUID is required" });
   const response = await fetch(
     `https://api.hypixel.net/v2/skyblock/profiles?uuid=${uuid}`,
